2099050a48495f93d156c1870a9d1779512223a6
Morozov-it <morozov.it2020@gmail.com>
2022-01-25 09:47:22 +0300

    Create routing into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Header = () => (<h1>Header</h1>)
+const Step1 = () => (<>Step1</>)
+const Step2 = () => (<>Step2</>)
+const Step3 = () => (<>Step3</>)
+const Result = () => (<>Result</>)
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Step1 />} />
+        <Route path="/step2" element={<Step2 />} />
+        <Route path="/step3" element={<Step3 />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
