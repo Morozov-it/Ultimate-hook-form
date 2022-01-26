fd9630bcf534a6d568b6be07bfa4d0b72331a4e8
Morozov-it <morozov.it2020@gmail.com>
2022-01-26 11:10:58 +0300

    Add table in page result

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -5,10 +5,14 @@ import { useNavigate } from "react-router-dom";
 //файлы библиотек
 import Typography from '@material-ui/core/Typography';
 import Article from '@mui/icons-material/Article';
+import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
-import Paper from '@material-ui/core/Paper';
+import { Table, TableCell, TableRow } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListItemIcon, Paper  } from '@material-ui/core';
+import { InsertDriveFile } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
 //компоненты
 import { MainContainer } from '../components/MainContainer';
 import { MyForm } from '../components/MyForm'
@@ -16,15 +20,35 @@ import { MyButton } from '../components/MyButton';
 //получение данных из контекста
 import { useData } from '../DataContext'
 
+
+const useStyles = makeStyles({
+    title: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    table: {
+        marginBottom: '1rem'
+    }
+});
+
+
 export const Result = () => {
+    const styles = useStyles();
     const { data, setData } = useData();
     let navigate = useNavigate();
+
+    //фильтрация данных в объекте data по ключу file
+    const entries = Object.entries(data).filter((entry) =>
+        entry[0] !== 'file' && entry[1] !== undefined && entry[0] !== 'havePhone'
+    );
+    const { file } = data;
 
     const { handleSubmit } = useForm({
         defaultValues: {...data},
     });
 
     const onSubmit = async (formData) => {
+        
         console.log('sent to server', formData);
         //отправить на сервер (formData)
     };
@@ -37,19 +61,58 @@ export const Result = () => {
     return (
         <MainContainer>
             <Typography
+                className={styles.title}
                 variant="h5"
                 component="h2">
-                <Article color='primary'/>
-                Form values
+                <Article  color='primary'/>
+                <span> Form values</span>
             </Typography>
-            <TableContainer component={Paper}>
-                {/* <TableHead>
-
-                </TableHead>
-                <TableBody>
-
-                </TableBody> */}
+            <TableContainer className={styles.table} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                            Field
+                            </TableCell>
+                            <TableCell align='right'>
+                            Value
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {entries.map((entry) =>
+                            <TableRow key={entry[0]}>
+                                <TableCell>
+                                    {entry[0]}
+                                </TableCell>
+                                <TableCell align='right'>
+                                    {entry[1].toString()}
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
             </TableContainer>
+            {file &&
+            <>
+                <Typography
+                    className={styles.title}
+                    variant="h5"
+                    component="h2">
+                    <ForwardToInboxIcon />
+                    <span> Files</span>
+                </Typography>
+                <List>
+                    {file.map((i, index) =>
+                        <ListItem key={index}>
+                            <ListItemIcon>
+                                <InsertDriveFile />
+                            </ListItemIcon>
+                            <ListItemText primary={i.name} secondary={i.size}/>
+                        </ListItem>
+                    )}
+                    </List>
+            </>}
             <MyForm onSubmit={handleSubmit(onSubmit)}>
                 <MyButton
                     type='submit'
